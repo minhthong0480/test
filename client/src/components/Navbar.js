9ea5829609592d4ec78011ea0cfd1085420c44ff
Minh Thong <s3852882@rmit.edu.vn>
2022-09-15 14:59:04 +0700

    weqwe

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
       <div className="nav bg-light d-flex justify-content-between">
         <Link className='nav-link' to='/'>Home</Link>
         <Link className='nav-link' to='/login'>Login</Link>
-        <Link className='nav-link' to='/register'>Register</Link>
+        {/* <Link className='nav-link' to='/register'>Register</Link> */}
       </div>
     </Fragment>
   )
